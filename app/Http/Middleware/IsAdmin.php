467f5e107a928d93f55a022a89e7ed0885559a60
Caroline Altos <carolinests99@gmail.com>
2024-09-10 11:35:00 -0300

    feat: adiciona middleware de níveis de permissão

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsAdmin
+{
+    const ADMIN = 'admin';
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        //return $next($request);
+
+        if (Auth::user()->role !== self::ADMIN) {
+            return response()->json('Opps! You do not have permission to access.');
+        }
+
+        return $next($request);
+    }
+}
